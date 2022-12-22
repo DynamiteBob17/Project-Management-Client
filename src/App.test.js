15749c9469d4a2bd902a9dafebd5ffe5e54f9b81
PickleEaterJim33 <jan.mlinaric@gmail.com>
2022-12-22 02:14:51 +0100

    test: just a simple test to see how jest works
    and modify package.json

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders Logout button', () => {
+  render(<App />);
+  const logoutElement = screen.getByText('LOGIN');
+  expect(logoutElement).toBeInTheDocument();
+});
--- src/components/interface/analytics/Analytics.jsx
+++ src/components/interface/analytics/Analytics.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { PieChart } from "react-minimal-pie-chart"; 
 
 function Analytics(props) {
+    const [taskStatuses, setTaskStatuses] = useState([1, 1, 1]);
+    const [taskPriorities, setTaskPriorities] = useState([1, 1, 1]);
+
+    useEffect(() => {
+
+    }, [props.tasks, taskStatuses, taskPriorities]);
+
     return (
         <div id="analytics">
-            <h1>Analytics</h1>
+            <PieChart
+                data={[
+                    { title: "Completed", value: taskStatuses[0], color: "lime" },
+                    { title: "Incomplete", value: taskStatuses[1], color: "orange" },
+                    { title: "Overdue", value: taskStatuses[2], color: "red" },
+                ]}
+            />
+            <PieChart
+                data={[
+                    { title: "Completed", value: taskPriorities[0], color: "lime" },
+                    { title: "Incomplete", value: taskPriorities[1], color: "orange" },
+                    { title: "Overdue", value: taskPriorities[2], color: "red" },
+                ]}
+            />
         </div>
     );
 }
