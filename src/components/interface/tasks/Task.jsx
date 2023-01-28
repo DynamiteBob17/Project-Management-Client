155f03a0b3648af9cfd78833fc2b1256e13c7a7b
PickleEaterJim33 <jan.mlinaric@gmail.com>
2023-01-28 18:15:47 +0100

    fix: eslint errors #3

--- src/components/interface/tasks/Task.jsx
+++ src/components/interface/tasks/Task.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 import ShowDescModal from "./ShowDescModal";
 import ViewMembersModal from "./ViewMembersModal";
 import CommentsModal from "./CommentsModal";
